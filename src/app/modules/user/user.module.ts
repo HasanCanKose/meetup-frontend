82d7ac4577faf9813220d3fa09db7c093728c2af
Hasan Can Kose <hasancannkosee@gmail.com>
2020-11-23 04:38:15 +0300

    Add user-profile component

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserProfileComponent],
   imports: [
     CommonModule
   ]
