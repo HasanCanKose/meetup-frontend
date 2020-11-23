4f406b4c9e074882e277c43932d7ecc9dd71e014
Hasan Can Kose <hasancannkosee@gmail.com>
2020-11-23 04:34:30 +0300

    Add event-form component

--- src/app/modules/event/event.module.ts
+++ src/app/modules/event/event.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventFormComponent } from './event-form/event-form.component';
 
 
 
 @NgModule({
-  declarations: [EventDetailComponent],
+  declarations: [EventDetailComponent, EventFormComponent],
   imports: [
     CommonModule
   ]
