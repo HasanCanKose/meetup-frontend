11be302a67fd143e92a99b74b864cd930333cacb
Hasan Can Kose <hasancannkosee@gmail.com>
2020-11-23 04:39:00 +0300

    Add user-settings component

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 
 
 @NgModule({
-  declarations: [UserProfileComponent],
+  declarations: [UserProfileComponent, UserSettingsComponent],
   imports: [
     CommonModule
   ]
