5e5b5203fac9e4231ee96354527fb6f1773377f9
Hasan Can Kose <hasancannkosee@gmail.com>
2020-12-29 01:12:06 +0300

    Add username field and its styling

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -16,6 +16,7 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       firstname: new FormControl(null, Validators.required),
       lastname: new FormControl(null, Validators.required),
+      username: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
       confirmPassword: new FormControl(null, Validators.required)
