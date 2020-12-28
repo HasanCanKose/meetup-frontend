eb7b9b73ba6a646038370a8de591ece317787122
Hasan Can Kose <hasancannkosee@gmail.com>
2020-12-28 23:19:15 +0300

    Add form controls

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,15 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    })
   }
 
   onNavigateRegisterPage() {
