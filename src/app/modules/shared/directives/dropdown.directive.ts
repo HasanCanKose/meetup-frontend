e38d7a3c0aeddf8fd648bcdcb0a409c2e190a87d
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-01 23:15:38 +0300

    Add dropdown menu on header and its directive

---
+++ src/app/modules/shared/directives/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document: click', ['$event']) toggleClick(event: Event){
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+  constructor(private elRef: ElementRef) {
+  }
+
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import {EventCardAnimationDirective} from './directives/eventcard-animation.directive';
+import { DropdownDirective } from './directives/dropdown.directive';
 
 
 
 @NgModule({
-    declarations: [ButtonComponent, EventCardAnimationDirective],
+    declarations: [ButtonComponent, EventCardAnimationDirective, DropdownDirective],
 
     imports: [
         CommonModule
     ],
-  exports: [
-    ButtonComponent,
-    EventCardAnimationDirective
-  ]
+    exports: [
+        ButtonComponent,
+        EventCardAnimationDirective,
+        DropdownDirective
+    ]
 })
 export class SharedModule { }
