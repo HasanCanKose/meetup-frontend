edb4e2cd8bd18927ea95f4695eb01d4b8c3eca1d
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 18:18:10 +0300

    Refactor auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,22 +2,20 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
-import {UserService} from '../services/user.service';
-import {mapTo} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router, private userService: UserService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     if(this.authService.loggedIn()) {
-      return this.userService.getUser().pipe(mapTo(true));
+      return true;
     } else {
       return this.router.parseUrl("/login");
     }
