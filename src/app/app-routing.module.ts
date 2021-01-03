c0c9db5db48a193d8b772b8dd8fa509fe9ccad2f
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 18:35:43 +0300

    Add prettierrc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EventListComponent } from './modules/event/event-list/event-list.component';
-import {LoginComponent} from './modules/auth/login/login.component';
-import {RegisterComponent} from './modules/auth/register/register.component';
-import {AuthGuard} from './guards/auth.guard';
-import {UserProfileComponent} from './modules/user/user-profile/user-profile.component';
-import {EventsResolver} from './resolvers/events.resolver';
-import {UserResolver} from './resolvers/user.resolver';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
+import { UserProfileComponent } from './modules/user/user-profile/user-profile.component';
+import { EventsResolver } from './resolvers/events.resolver';
+import { UserResolver } from './resolvers/user.resolver';
 
 const appRoutes: Routes = [
-  {path: '', resolve: [UserResolver],
+  {
+    path: '',
+    resolve: [UserResolver],
     children: [
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'events', component: EventListComponent, canActivate: [AuthGuard], resolve: {events: EventsResolver} },
-      {path: 'profile/:id', component: UserProfileComponent}
-    ]},
-  { path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-
+      { path: 'events', component: EventListComponent, canActivate: [AuthGuard], resolve: { events: EventsResolver } },
+      { path: 'profile/:id', component: UserProfileComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
