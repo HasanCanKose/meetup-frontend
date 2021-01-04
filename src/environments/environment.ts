92b005cae059c5fe7b2ebe3763e57179e92804db
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-05 01:25:41 +0300

    Refactor environment variable for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://meetupp-app.herokuapp.com',
+  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'https://meetupp-app.herokuapp.com',
 };
 
 /*
