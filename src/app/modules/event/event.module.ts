1f723512ab69f5f334161d102d1fe46dd18c1f3c
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-06 01:19:55 +0300

    Delete user settings and event form components

--- src/app/modules/event/event.module.ts
+++ src/app/modules/event/event.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDetailComponent } from './event-detail/event-detail.component';
-import { EventFormComponent } from './event-form/event-form.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [EventDetailComponent, EventFormComponent, EventListComponent],
+  declarations: [EventDetailComponent, EventListComponent],
   imports: [CommonModule, SharedModule],
 })
 export class EventModule {}
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserSettingsComponent } from './user-settings/user-settings.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [UserProfileComponent, UserSettingsComponent],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ]
+  declarations: [UserProfileComponent],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
 })
-export class UserModule { }
+export class UserModule {}
